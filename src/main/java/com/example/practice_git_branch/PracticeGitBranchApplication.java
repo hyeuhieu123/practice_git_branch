f0d719125e80d21fc45fdee0ad214b60f84146fd
hieu <nguyentrunghieuthptxp@gmail.com>
2025-08-12 18:21:55 +0700

    init

---
+++ src/main/java/com/example/practice_git_branch/PracticeGitBranchApplication.java
@@ -0,0 +1,13 @@
+package com.example.practice_git_branch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeGitBranchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeGitBranchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/practice_git_branch/PracticeGitBranchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practice_git_branch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeGitBranchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
